2d0d1add3777296edff722498d156cb38c3ab1c3
harrish1709 <150880592+harrish1709@users.noreply.github.com>
2025-11-19 22:06:51 +0530

    Update sharafdg_scraper.py

--- scrapers/sharafdg_scraper.py
+++ scrapers/sharafdg_scraper.py
@@ -14,11 +14,15 @@ def scrape_sharafdg(brand, product, oem_number=None, asin_number=None):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--window-size=1920,1080")
-    options.add_argument(
+    # Random User Agent
+    user_agents = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15"
-    )
+    ]
+    
+    options.add_argument(f"--user-agent={random.choice(user_agents)}")
+
 
     driver = uc.Chrome(options=options)
 

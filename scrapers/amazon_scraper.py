2cb05f90d767bdbc4d929e2e96b1ca3439f58f78
harrish1709 <150880592+harrish1709@users.noreply.github.com>
2025-11-22 12:14:27 +0530

    Update amazon_scraper.py

--- scrapers/amazon_scraper.py
+++ scrapers/amazon_scraper.py
@@ -64,7 +64,7 @@ def scrape_amazon(brand, product):
             try:
                 options = uc.ChromeOptions()
                 if headless:
-                    options.add_argument("--headless+new")
+                    options.add_argument("--headless=new")
                     options.add_argument("--no-sandbox")
                     options.add_argument("--disable-dev-shm-usage")
                     options.add_argument("--disable-gpu")
